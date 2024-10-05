374502224f2cb3421526239a42efb3b53807e090
Rory Dowse <rorydowse@hotmail.com>
2024-10-05 11:34:54 -0700

    update userRoutes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { userRouter } from './users.js';
-import { thoughtRouter } from './thoughts.js';
+import { userRouter } from './userRoutes.js';
+import { thoughtRouter } from './thoughtRoutes.js';
 
 const router = Router();
 
---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'; // Import Router from express
+const router = Router(); // Create a new router instance
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} from '../../controllers/userController.js'; // Import user-related controller functions
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:id
+router
+    .route('/:userId')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser); // ensure controller removes user thoughts
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend).delete(removeFriend);
+
+export { router as userRouter };
